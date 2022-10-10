8daaf2ac07d2c750a6d36acfad12d7a0e35fb463
aminakhatun22 <aminak26802@gmail.com>
2022-10-11 02:56:02 +0600

    added-topics

---
+++ src/components/Routes/routes.js
@@ -0,0 +1,53 @@
+import { createBrowserRouter } from "react-router-dom"
+import Root from "../Root/Root"
+import Home from "../Home/Home"
+// import Errorpage from '../Errorpage/Errorpage'
+import Blog from '../Bolg/Blog'
+import Errorpage from '../Errorpage/Errorpage'
+import Statics from '../Statics/Statics'
+import Topics from '../Topics/Topics'
+
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root></Root>,
+        errorElement: <Errorpage></Errorpage>,
+
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>,
+
+            },
+            {
+                path: '/home',
+                loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+                element: <Home></Home>
+
+            },
+            {
+                path: '/topics',
+                element: <Topics></Topics>,
+
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+
+            {
+                path: '/statics',
+                element: <Statics></Statics>
+            }
+
+
+        ]
+    },
+
+
+
+
+])
+
+export default router
